a375e33117468792fc06ebb61458a401a83c6c66
Ibrahim60 <Ibrahim.cs2017@gmail.com>
2020-08-18 14:25:50 +0500

    Update the html file and also refined the script

--- Keyboard-Event/script.js
+++ Keyboard-Event/script.js
@@ -1,84 +1,93 @@
 window.addEventListener('keypress',checkKey);
-function checkKey(num){
-    var digit,digit1,digit2 = 0;
-    var oper,key,isFirst = " ";
-    console.log(digit);
-    console.log(isFirst);
-    isFirst = true;
-    console.log(isFirst);
-    if(isFirst===true){
+var isFirst = true;
+function checkKey(key){
+    console.log(key.keyCode);
+    var num,digit,digit1,digit2 = 0;
+    var oper= " ";
+    num = parseInt(key.keyCode);
+    if(isFirst === true){
         digit1 = oprend(num, digit);
         console.log("isFirst if true "+digit1);
-    }else if(isFirst===false){
+    }else if(isFirst === false){
         digit2 = oprend(num, digit);
         console.log("isFirst if false "+digit2);
-    }
-    console.log(digit1+digit2);
-    oper = operator(num,key);
-    calculation(oper,digit1,digit2);
+    }else if(digit1 != 0){
+        oper = operator(num);
+    }else if (digit1 != 0 && digit2 != 0 && oper != " "){
+        calculation(oper,digit1,digit1);
+        console.log(`Calculation oper${oper} digit1${digit1} digit2${digit2}`);
+    } 
 }
 
-function oprend(num,key) {
+function oprend(num,digit) {
     switch (num.keyCode) {
         case 48:
-            key = 0;
-            console.log(key);
+            digit = 0;
+            console.log(digit);
             break;
         case 49:
-            key = 1;
+            digit = 1;
             break;
         case 50:
-            key = 2;
+            digit = 2;
             break;
         case 51:
-            key = 3;
+            digit = 3;
             break;
         case 52:
-            key = 4;
+            digit = 4;
             break;
         case 53:
-            key = 5;
+            digit = 5;
             break;
         case 54:
-            key = 6;
+            digit = 6;
             break;
         case 55:
-            key = 7;
+            digit = 7;
             break;
         case 56:
-            key = 8;
+            digit = 8;
             break;
         case 57:
-            key = 9;
+            digit = 9;
             break;
         default:
-            console.log("Please Enter A Num");
+            console.log("Please enter a num");
             break;
     }
+    // if(isFirst===true){
+    //     isFirst = false;
+    // } else if(isFirst===false){
+    //     isFirst = true;
+    // }
     isFirst = false;
-    console.log(isFirst);
-    return key;
+    return digit;
 }
 
-function operator(num,digit){
+function operator(num,key){
     switch (num.keyCode) {
         case 42:
-            digit = '*';
+            key = '*';
+            console.log("*");
             break;
         case 43:
-            digit = '+';
+            key = '+';
+            console.log("+");
             break;
-        case 44:
-            digit = '+';
+        case 45:
+            key = '-';
+            console.log("-");
             break;
         case 47:
-            digit = '/';
+            key = '/';
+            console.log("/");
             break;
         default:
             console.log("Please enter a valid operator");
             break;
     }
-    return digit;
+    return key;
 }
 
 function calculation(oper,digit1,digit2){
@@ -103,7 +112,7 @@ function calculation(oper,digit1,digit2){
 
 // 42 * (Asterisk)
 // 43 + (Plus sign)
-// 44 , (Comma)
+// 45 - (Hyphen)
 // 47 / (Slash)
 // 48 0 (number zero)
 // 49 1 (number one)
